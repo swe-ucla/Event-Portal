3fc50daa3cc3b6ed2f31c49c462075b0be689df6
yuyingchua <ychua@ucla.edu>
2019-04-04 16:37:02 -0700

    Added /events endpoint

--- server/routes/events.js
+++ server/routes/events.js
@@ -2,14 +2,30 @@
 var express = require('express');
 var router = express.Router();
 
-// Returns test string to verify that Events server is running. 
-router.get('/ping', function(req, res, next) {
-  res.send('testing change');
+// Require database adapter file (not node-postgres directly)
+const db = require('../db')
+
+// Get all events
+router.get('/', function(req, res, next) {
+  db.query('SELECT * FROM event', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
 });
 
 // GET events listing. 
-router.get('/', function(req, res, next) {
-    // TODO
-});
+// router.get('/filter', function(req, res, next) {
+//   // const date = req.query.date;
+
+
+//   db.query('SELECT fb_id FROM event WHERE date(starts_at) <= \'' + date + '\' AND \'' + date + '\' <= date(ends_at)', [], (err, result) => {
+//     if (err) {
+//         return next(err);
+//     }
+//     res.send(result.rows);
+//   });
+// });
 
 module.exports = router;
