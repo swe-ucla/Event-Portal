f080e5cf27eda74a1a72b5d51d16400ecf041506
Zian Chen <zianchen774@gmail.com>
2019-04-04 16:55:18 -0700

    reverted to correct version

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,26 +1,84 @@
-/* Route Prefix: /events */
+//* Route Prefix: /events */
 var express = require('express');
 var router = express.Router();
 
 // Require database adapter file (not node-postgres directly)
 const db = require('../db')
 
-// Get all events
-router.get('/', function(req, res, next) {
-  db.query('SELECT * FROM event', [], (err, result) => 	{
-    	if (err) {
-      	  return next(err);
-    	}
-    	res.send(result.rows);
-  	});
+//Get all users checked in to a given event
+router.get('/:event_id/checkin', function(req, res, next) {
+  const event_id = req.params.event_id;
+  db.query('SELECT user_id FROM event_checkin WHERE event_id = \'' + event_id + '\'', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+  
 });
 
+//Get all hosts for a given event
+router.get('/:event_id/host', function(req, res, next) {
+  const event_id = req.params.event_id;
+  db.query('SELECT host_id FROM event_host WHERE event_id = \'' + event_id + '\'', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+  
+});
+
+//Get all companies for a given event
+router.get('/:event_id/companies', function(req, res, next) {
+  const event_id = req.params.event_id;
+  db.query('SELECT company_id FROM event_company WHERE event_id = \'' + event_id + '\'', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+  
+});
+
+//Get all categories for a given event
+router.get('/:event_id/categories', function(req, res, next) {
+  const event_id = req.params.event_id;
+  db.query('SELECT category_id FROM event_category WHERE event_id = \'' + event_id + '\'', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+  
+});
+
+//Get all events containing term substring
+router.get('/search', function(req, res, next) {
+	const term = req.query.term;
+  db.query('SELECT * FROM event WHERE name ILIKE \‘%<\'' + term + '\'>%\’', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+ 
 // Get all users registered to a given event
 router.get('/:event_id/register', function(req, res, next) {
 	const event = req.params.event_id;
 	db.query('SELECT user_id FROM event_registration WHERE event_id = \'' + event + '\'', [], (err, result) => {
 		if (err) {
         	return next(err);
+    	}
+    	res.send(result.rows);
+  	});
+});
+
+// Get all events
+router.get('/', function(req, res, next) {
+  db.query('SELECT * FROM event', [], (err, result) => 	{
+    	if (err) {
+      	  return next(err);
     	}
     	res.send(result.rows);
   	});
@@ -52,13 +110,19 @@ router.get('/locations', function(req, res, next) {
 //   if (date != undefined) {
 
 //   }
+// });
 
-	// db.query('SELECT fb_id FROM event WHERE date(starts_at) <= \'' + date + '\' AND \'' + date + '\' <= date(ends_at)', [], (err, result) => {
- //    if (err) {
- //        return next(err);
- //    }
- //    res.send(result.rows);
- //  });
+// GET events listing. 
+// router.get('/filter', function(req, res, next) {
+//   // const date = req.query.date;
+
+
+//   db.query('SELECT fb_id FROM event WHERE date(starts_at) <= \'' + date + '\' AND \'' + date + '\' <= date(ends_at)', [], (err, result) => {
+//     if (err) {
+//         return next(err);
+//     }
+//     res.send(result.rows);
+//   });
 // });
 
 module.exports = router;
