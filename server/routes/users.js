9b9fc88b38c00a476e36bab55b0e716709c0dee4
nikhitashanker <rocketns@gmail.com>
2019-04-08 15:50:04 -0700

    added more routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -59,18 +59,133 @@ router.get('/ids', function(req, res, next) {
   });
 });
 
-//GET all unique event locations.
 
-
+//Get user info by user_id
 router.get('/:user_id', function(req, res, next) {
   const id = req.params.user_id;
-  db.query('SELECT * FROM swe_user WHERE id = user_id', [id], (err, result) => {
+  db.query('SELECT * FROM swe_user WHERE id = $1', [id], (err, result) => {
     if (err) {
         return next(err);
     }
     res.send(result.rows);
   });
 });
+
+//Get whether user is admin or not
+router.get('/:user_id/admin', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT is_admin FROM swe_user WHERE id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get a user's past events-HELP NEEDED
+router.get('/:user_id/past', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT event_id FROM event_checkin INNER JOIN event ON event_checkin.event_id = event.fb_id WHERE event_checkin.user_id = 3 AND event.ends_at < now() UNION SELECT event_id FROM event_registration INNER JOIN event ON event_registration.event_id = event.fb_id WHERE event_registration.user_id = 3 AND event.ends_at < now()', [], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get all companies a user is interested in
+router.get('/:user_id/companies', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT company_id, rank FROM user_company_rank WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+
+//Get all events a user is attending
+router.get('/:user_id/events', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT event_id FROM event_checkin WHERE user_id = $1 UNION SELECT event_id FROM event_registration WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get all events a user is hosting
+router.get('/:user_id/host', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT event_id FROM event_host WHERE host_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get all the user's majors
+router.get('/:user_id/majors', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT major_id FROM user_major WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get all positions a user is seeking
+router.get('/:user_id/positions', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT position_id FROM user_position WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get all the user's occupations
+router.get('/:user_id/occupations', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT occupation_id FROM user_occupation WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+
+//Get the user's diet information
+router.get('/:user_id/diet', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT diet_id FROM user_diet WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+//Get a user's favorite events
+router.get('/:user_id/favorite', function(req, res, next) {
+  const id = req.params.user_id;
+  db.query('SELECT event_id FROM favorite_events WHERE user_id = $1', [id], (err, result) => {
+    if (err) {
+        return next(err);
+    }
+    res.send(result.rows);
+  });
+});
+
+
+
+
 
 
 
