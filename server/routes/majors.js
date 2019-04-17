21fdfd7a899000b05c28958c607b5fcdb29ac562
Katrina Wijaya <katrina@thewijayas.com>
2019-04-17 04:43:15 -0700

    Preliminary implementation of PPD majors routes. TODO: knexjs

--- server/routes/majors.js
+++ server/routes/majors.js
@@ -21,4 +21,32 @@ router.get('/names', function(req, res, next) {
   });
 });
 
+// Add a single major
+router.post('/', function(req, res, next) {
+  values = [ req.query.name, req.query.ucla_id ];
+  db.query('INSERT INTO major (name, ucla_id) VALUES ($1, $2)', values, (err, result) => {
+    if (err) return next(err);
+    res.send("Successfully inserted new major: " + req.query.name);
+  });
+});
+
+// Update a single major
+// TODO: https://knexjs.org/
+router.put('/:major_id', function(req, res, next) {
+  values = [ req.params.major_id, req.query.name, req.query.ucla_id ];
+  db.query('UPDATE major SET name = $2, ucla_id = $3 WHERE id = $1', values, (err, result) => {
+    if (err) return next(err);
+    res.send("Successfully updated major: " + req.query.name);
+  });
+});
+
+// TODO: Delete a single major
+router.delete('/:major_id', function(req, res, next) {
+  const major_id = req.params.major_id;
+  db.query('DELETE FROM major WHERE id = $1', [major_id], (err, result) => {
+    if (err) return next(err);
+    res.send("Successfully deleted major: " + req.params.major_id);
+  });
+});
+
 module.exports = router;
