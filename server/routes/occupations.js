5533826817131cb7b3c5c0e3e7da584373f74718
yuyingchua <ychua@ucla.edu>
2019-04-27 16:20:30 -0700

    Added PUT and DELETE methods to occupations endpoint

--- server/routes/occupations.js
+++ server/routes/occupations.js
@@ -19,4 +19,33 @@ router.get('/', function(req, res, next) {
     .catch(err => { return next(err) });
 });
 
+// Update a single occupation
+router.put('/:occupation_id', function(req, res, next) {
+  const occupation_id = req.params.occupation_id;
+  values = { name: req.query.name, user_id: req.query.user_id };
+  knex('occupation').update(values).where({ id: occupation_id })
+    .then(result => {
+      if (result) {
+        res.send(util.message('Successfully updated occupation: ' + occupation_id));
+      } else {
+        util.throwError(404, 'No occupation found to update');
+      }
+    })
+    .catch(err => { return next(err) });
+});
+
+// Delete a single occupation
+router.delete('/:occupation_id', function(req, res, next) {
+  const occupation_id = occupation_id;
+  knex('occupation').del().where({ id: occupation_id })
+    .then(result => {
+      if (result) {
+        res.send(util.message('Successfully deleted occupation: ' + occupation_id));
+      } else {
+        util.throwError(404, 'No occupation found to delete');
+      }
+    })
+    .catch(err => { return next(err) });
+});
+
 module.exports = router;
