8041eb05ee745fb7994721eb390afbf5129de785
nikhitashanker <rocketns@gmail.com>
2019-04-27 09:13:11 -0700

    Finished  PUT/POST/GET queries except updating multiple tables on registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -65,24 +65,13 @@ router.get('/ids', function(req, res, next) {
 });
 
 //Add a user
-router.get('/:user_id/id', function(req, res, next) {
-  const user_id = req.params.user_id;
-  knex('swe_user').select()
-  .where( {id: user_id} )
-  .then(result => {
-      if (result.length) {
-        res.json(result);
-      } else {
-        util.throwError(404, 'No users matching user_id = ' + user_id + ' found');
-      }
-    })
-    .catch(err => { return next(err) 
-  });
-});
-  
-
-//Login a user
 router.post('/register', function(req, res, next) {
+  if (!req.query.first_name) {
+    util.throwError(400, 'First name must not be null');
+  }
+  if (!req.query.last_name) {
+    util.throwError(400, 'First name must not be null');
+  }
   values =
   {
     id: req.query.id,
@@ -97,6 +86,24 @@ router.post('/register', function(req, res, next) {
   knex('swe_user').insert(values)
     .then(result => {
       res.send(util.message('Successfully inserted new user: ' + req.query.first_name));
+    })
+    .catch(err => { return next(err) });
+});
+  
+
+//Login a user
+//Unsuccessful on postman, error updated_at does not exist
+router.put('/login', function(req, res, next) {
+  knex('swe_user')
+    .update ({
+        update_at: knex.fn.now(),
+    })
+    .where({ 
+        email: req.query.email, 
+        password: req.query.password,
+    })
+    .then(result => {
+      res.send(util.message('Successfully logged in user with email: ' + req.query.email));
     })
     .catch(err => { return next(err) });
 });
@@ -136,10 +143,9 @@ router.get('/:user_id/admin', function(req, res, next) {
 });
 
 //Update user info
-router.put('/:event_id', function(req, res, next) {
+router.put('/:user_id', function(req, res, next) {
   values =
   {
-    id: req.query.id,
     first_name: req.query.first_name,
     last_name: req.query.last_name,
     password: req.query.password,
@@ -148,7 +154,7 @@ router.put('/:event_id', function(req, res, next) {
     university_id: req.query.university_id,
     is_admin: req.query.is_admin,
   }
-  knex('swe_user').update(values).where({ id: req.params.id })
+  knex('swe_user').update(values).where({ id: req.params.user_id })
     .then(result => {
       res.send(util.message('Successfully updated user: ' + req.query.first_name));
     })
@@ -250,8 +256,38 @@ router.get('/:user_id/favorite', function(req, res, next) {
 });
 
 //Add a favorite event to a user
+router.post('/:user_id/favorite/:event_id', function(req, res, next) {
+  if (!req.params.event_id) {
+    util.throwError(400, 'Event ID must not be null');
+  }
+  if (!req.params.user_id) {
+    util.throwError(400, 'User ID must not be null');
+  }
+  values =
+  {
+    event_id: req.params.event_id,
+    user_id: req.params.user_id,
+  }
+  knex('favorite_events').insert(values)
+    .then(result => {
+      res.send(util.message('Successfully inserted new favorite event: ' + req.params.event_id + ' for user ' + req.params.user_id));
+    })
+    .catch(err => { return next(err) });
+});
 
 //Delete a favorite event from a user
+router.delete('/:user_id/favorite/:event_id', function(req, res, next) {
+  values =
+  {
+    event_id: req.params.event_id,
+    user_id: req.params.user_id,
+  }
+  knex('favorite_events').del().where(values)
+    .then(result => {
+      res.send(util.message('Successfully deleted favorite event: ' + req.params.event_id + ' for user ' + req.params.user_id));
+    })
+    .catch(err => { return next(err) });
+});
 
 // GET a user's favorite events
 router.get('/search', function(req, res, next) {
