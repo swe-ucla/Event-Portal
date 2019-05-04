f5d566d4c4deb440ab983bc131906d1d457145a0
amysywe <source016@gmail.com>
2019-05-04 13:29:55 -0700

    added POST /positions/ and PUT /positions/:position_id

--- server/routes/positions.js
+++ server/routes/positions.js
@@ -19,6 +19,48 @@ router.get('/', function(req, res, next) {
     .catch(err => { return next(err) });
 });
 
+// Add a single position
+router.post('/', function(req, res, next) {
+  if (!req.query.role) {
+    util.throwError(400, 'Position role must not be null');
+  }
+  
+  knex('position').insert({ role: req.query.role })
+    .then(result => {
+      res.send(util.message('Successfully inserted new position: ' + req.query.role));
+    })
+    .catch(err => { return next(err) });
+});
+
+// Update a single position
+router.put('/:position_id', function(req, res, next) {
+  let errormsg = 'The following fields must not be null: ';
+  let validReq = true;
+
+  if (!req.params.position_id){
+    errormsg += 'position_id, ';
+    validReq = false;
+  }
+  if (!req.query.role){
+    errormsg += 'role, ';
+    validReq = false;
+  }
+
+  if (!validReq){
+    util.throwError(400, errormsg.substring(0, errormsg.length - 2));
+  }
+
+  knex('position').update({ role: req.query.role }).where({ id: req.params.position_id })
+    .then(result => {
+      if (result) {
+        res.send(util.message('Successfully updated position: ' + req.params.position_id));
+      } else {
+        util.throwError(404, 'No position found to update');
+      }
+    })
+    .catch(err => { return next(err) });
+});
+
 // Delete a single position
 router.delete('/positions/:position_id', function(req, res, next) {
   const position_id = req.params.position_id;
