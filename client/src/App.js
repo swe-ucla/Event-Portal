a73794b5b8567462f06a8461f7bc059c4ee88e76
Katrina Wijaya <katrina@thewijayas.com>
2019-05-06 22:51:53 -0700

    Partially working Google Analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 import NavBar from './components/NavBar.js'
 import Checkout from './components/Checkout.js';
@@ -9,16 +11,35 @@ import ExampleDeleteForm from './components/ExampleDeleteForm.js'
 
 require('typeface-roboto');
 
+ReactGA.initialize('UA-139728260-1', { debug: true });
+// ReactGA.initialize('UA-139728260-1');
+
+const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+  // ReactGA.event({
+  //   category: "Pageview",
+  //   action: "Page: " + location.pathname + location.search,
+  //   label: "Page: " + location.pathname + location.search,
+  // });
+});
+
 class App extends Component {
+  componentWillMount() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <NavBar />
         <Route path="/checkout" component={Checkout} />
         <Route path="/post" component={ExamplePostForm} />
         <Route path="/put" component={ExamplePutForm} />
         <Route path="/delete" component={ExampleDeleteForm} />
-      </BrowserRouter>
+      </Router>
     )
   }
 }
