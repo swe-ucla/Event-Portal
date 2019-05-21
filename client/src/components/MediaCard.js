27919ed319a364f8abff1de439302f7063d18388
amysywe <source016@gmail.com>
2019-05-20 19:13:51 -0700

    updated MediaCard

--- client/src/components/MediaCard.js
+++ client/src/components/MediaCard.js
@@ -8,15 +8,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import '../css/MediaCard.css'
 
 const styles = {
   card: {
     width: 300,
-    padding: 10,
   },
   media: {
-    height: 100,
+    height: 200,
     width: 300,
   }
 };
@@ -31,14 +31,26 @@ function MediaCard(props) {
           image={require("./amy.jpg")}
           title="Contemplative Reptile"
         />
-        <CardContent>
-          <p className='event-name'>
-            AMY'S BABY SHOWER
-          </p>
-          <p className='description'>
-            have a swell time at amy's baby shower! celebrating her expansion of the waist
-          </p>
-        </CardContent>
+        <Grid container alignItems="center">
+          <Grid item xs={3} style={{textAlign: "center"}}>
+              <Typography component="h2" variant="body1">
+                JUNE
+              </Typography>
+              <Typography component="h5" variant="headline">
+                26
+              </Typography>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                AMY'S BABY SHOWER
+              </Typography>
+              <Typography component="p">
+                have a swell time at amy's baby shower! celebrating her expansion of the waist
+              </Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
