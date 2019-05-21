a3a3df2fef4a0c459115dd80c4558ca0b189e9c4
Zian Chen <zianchen774@gmail.com>
2019-05-20 19:06:32 -0700

    correct styling

---
+++ client/src/styles/MediaCard.js
@@ -0,0 +1,19 @@
+const MediaCardStyles = theme => ({
+	eventName: {
+		fontSize: '40px',
+		fontWeight: 'bold',
+	},
+	description: {
+		fontSize: '20px',
+	},
+	card: {
+    	width: '300px',
+    	padding: '10px',
+  	},
+  	media: {
+    	height: '100px',
+    	width: '300px',
+  	}
+})
+
+export default MediaCardStyles;
