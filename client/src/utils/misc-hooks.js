b858bbea9ba85c61f978ad8935cd7aa870cfe55a
Miles Wu <mileswu13@gmail.com>
2020-05-04 23:17:39 -0700

    Utils folder for reusable code
    Common folder accessible by all components intended for reusable
    logic. Custom hooks created for getting majors and occupations data from the
    database. These can now be used in any component that needs accesss to these
    pieces of data.

---
+++ client/src/utils/misc-hooks.js
@@ -0,0 +1,50 @@
+import {useState, useEffect} from 'react';
+
+export const useMajors = (initialState) => {
+    const [majors, setMajors] = useState(initialState);
+  
+    useEffect(() => {
+      axios.get('/majors')
+        .then(result => {
+          let major_data = {};
+          result.data.forEach((major) => {
+            major_data[major.id] = major.name;
+          });
+          console.log(major_data);
+          setMajors({...major_data});
+        })
+        .catch(err => console.log(err));
+      return () => {
+        setMajors({});
+      }
+    }, [])
+  
+    return {
+      majors
+    };
+  }
+  
+  export const useOccupations = (initialState) => {
+    const [occupations, setOccupations] = useState(initialState);
+  
+    useEffect(() => {
+      axios.get('/occupations')
+        .then(result => {
+          let occupation_data = {};
+          result.data.forEach((occupation) => {
+            occupation_data[occupation.id] = occupation.name
+          })
+          console.log(occupation_data);
+          setOccupations(occupation_data);
+        })
+        .catch(err => console.log(err));
+      return () => {
+        setOccupations({});
+      }
+    }, [])
+  
+    return {
+       occupations 
+    };
+  }
+  
