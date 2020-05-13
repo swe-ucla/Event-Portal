75009107a1305e3e90c735ee5ea1b90ef41a206a
Miles Wu <mileswu13@gmail.com>
2020-05-12 23:10:05 -0700

    Hooks for other database information
    Created shared hooks for other relevant information that is stored in the
    database. Includes diets, companies, and positions information.
    
    These hooks can now be used in any component that needs access to this
    information.
    
    TODO: Later can make these reducers when redux is integrated.

--- client/src/utils/misc-hooks.js
+++ client/src/utils/misc-hooks.js
@@ -34,7 +34,7 @@ export const useMajors = (initialState) => {
           let occupation_data = {};
           result.data.forEach((occupation) => {
             occupation_data[occupation.id] = occupation.name
-          })
+          });
           console.log(occupation_data);
           setOccupations(occupation_data);
         })
@@ -48,4 +48,76 @@ export const useMajors = (initialState) => {
        occupations 
     };
   }
-  
+
+export const useDiets = (initialState) => {
+  const [diets, setDiets] = useState(initialState);
+
+  useEffect(() => {
+    axios.get('/diet')
+      .then(result => {
+        let diet_data = {};
+        diet_data.forEach((diet) => {
+          diet_data[diet.id] = diet.type;
+        });
+        console.log(diet_data);
+        setDiets(diet_data);        
+      })
+      .catch(err => console.log(err));
+    return () => {
+      setDiets({});
+    }
+  }, [])
+
+  return {
+    diets
+  };
+}
+
+export const usePositions = (initialState) => {
+  const [positions, setPositions] = useState(initialState);
+
+  useEffect(() => {
+    axios.get('/positions')
+    .then(result => {
+      let position_data = {};
+      result.data.forEach((position) => {
+        position_data[position.id] = position.role;
+      });
+      console.log(position_data);
+      setPositions(position_data);
+
+    })
+    .catch(err => console.log(err));
+    return () => {
+      setPositions({});
+    }
+  }, [])
+
+  return {
+    positions
+  };
+}
+
+export const useCompanies = (initialState) => {
+  const [companies, setCompanies] = useState(initialState);
+
+  useEffect(() => {
+    axios.get('/companies')
+      .then(result => {
+        let companies_data = {};
+        result.data.forEach((company) => {
+          companies_data[company.id] = company.name;
+        });
+        console.log(companies_data);
+        setCompanies(companies_data);        
+      })
+      .catch(err => console.log(err));
+    return () => {
+      setCompanies({});
+    }
+  }, [])
+
+  return {
+    companies
+  };
+}
