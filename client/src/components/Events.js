d37d71e23ee05eeb4c707dcaefe1836950818bd6
Zian Chen <zianchen774@gmail.com>
2020-11-14 15:34:14 -0800

    Add event checkin feature

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -14,11 +14,13 @@ class Events extends Component {
     super();
 
     this.state = {
+      checkedIn: false,
+      date: "", // TO-DO: change to today's date. E.G.: 2019-03-02T15:30:00.000Z
       eventsByDayArray: {},
       loading: false,
-      date: "", // TO-DO: change to today's date. E.G.: 2019-03-02T15:30:00.000Z
-      prevY: 0,
       popUp: false,
+      prevY: 0,
+      userID: 0
     };
 
     this.getEvents = this.getEvents.bind(this);
@@ -28,6 +30,13 @@ class Events extends Component {
   }
 
   componentDidMount() {
+    // Get user ID for session
+    axios.get('/session')
+    .then(res => { console.log(res);
+    })
+    .catch(err => { console.log(err); })
+
+    // Load events onto screen
     this._isMounted = true;
     this.getEvents();
     let options = {
@@ -251,33 +260,29 @@ class Events extends Component {
     
     axios.get("/events", options)
     .then(result => {
-      let eventsData = result.data.map(function(event) {
-        return {
-          fb_id: event.fb_id,
-          name: event.name,
-          starts_at: event.starts_at,
-          ends_at: event.ends_at,
-          attendance_code: event.attendance_code,
-          location_id: event.location_id,
-          location: event.location_id,
-          description: event.description,
-          fb_event: event.fb_event,
-          picture: event.picture,
-          is_featured: event.is_featured,
-          updated_at: event.updated_at,
-          created_at: event.created_at,
-          period: event.period,
-          week: event.week,
-        };
-      });
+      let event_fb_id = result.data[0].fb_id;
+
+      // TO-DO: UI components for success/failed
       
-      // TO-DO: display "checked in"
-      // TO-DO: add to user's checked in list
-      console.log(eventsData);
+      // Checkin user based on event ID.
+      axios.post('/events/' + event_fb_id + '/checkin/' + this.state.userID)
+      .then(response => {
+        alert('Checked in successfully!');
+        this.setState({
+          checkedIn: true
+        });
+      })
+      .catch(error => {
+        alert('Already checked in!');
+        this.setState({ checkedIn: true });
+        console.log(error);
+      })
     })
     .catch(
-      // display not found icon
-      err => console.log(err)
+      error => {
+        alert('Wrong submission code!')
+        console.log(error)
+      }
     );
   }
 
@@ -300,11 +305,11 @@ class Events extends Component {
             </svg>
             <p className={classes.buttonLabel}>see past events</p>
           </button>
-          <button className={classes.checkInButton} onClick={this.handleOpenPopUp}>
+          <button disabled={this.state.checkedIn} className={classes.checkInButton} onClick={this.handleOpenPopUp}>
             Check In
           </button>
           <Modal open={this.state.popUp} onClose={this.handleClosePopUp} >
-            <PopUp onPopUpSubmit={this.handlePopUpSubmit}/>
+            <PopUp checkedIn={this.state.checkedIn} onPopUpSubmit={this.handlePopUpSubmit}/>
           </Modal>
           <div className={classes.events}>{this.renderEventRows()}</div>
           <div ref = {loadingRef => (this.loadingRef = loadingRef)} style={loadingCSS}>
