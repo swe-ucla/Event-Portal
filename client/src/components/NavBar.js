e04ab79d68d0a79f9607199b799268a237b3e07d
nikhitashanker <rocketns@gmail.com>
2020-11-14 10:42:10 -0800

    Add hook for user admin check and remove user summary from non-admin nav bar.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,33 +8,63 @@ import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
 import NavBarStyles from '../styles/NavBar.js'
+import { useAdmin } from '../utils/user-role-hooks.js';
 
 const NavBar = (props) => {
   const { classes } = props;
 
-  return(
-    <div>
-    <AppBar position="static" color="default" className={classes.appBar}>
-      <Toolbar>
-        <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-          Example Forms
-        </Typography>
-        <Button href="/">Home</Button>
-        <Button component={Link} to="/checkout">Checkout</Button>
-        <Button component={Link} to="/post">POST</Button>
-        <Button component={Link} to="/put">PUT</Button>
-        <Button component={Link} to="/delete">DELETE</Button>
-        <Button component={Link} to="/companies">Companies</Button>
-        <Button component={Link} to="/companiesform">Companies Form</Button>
-        <Button component={Link} to="/companiesadmin">Admin View</Button>
-        <Button component={Link} to="/events">EVENTS</Button>
-        <Button component={Link} to="/registerbasic">PROFILE</Button>
-        <Button component={Link} to="/registerEWI">PROFILE(EWI)</Button>
-        <Button component={Link} to="/users/admin">PROFILE SUMMARY(ADMIN)</Button>
-        <Button component={Link} to="/login" color="primary" variant="outlined">Login</Button>
-      </Toolbar>
-    </AppBar>
-    </div>
-  )
+  // GET information on whether current user is an admin
+  const { admin } = useAdmin({});
+
+  if (admin) {
+    return(
+      <div>
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+            Example Forms
+          </Typography>
+          <Button href="/">Home</Button>
+          <Button component={Link} to="/checkout">Checkout</Button>
+          <Button component={Link} to="/post">POST</Button>
+          <Button component={Link} to="/put">PUT</Button>
+          <Button component={Link} to="/delete">DELETE</Button>
+          <Button component={Link} to="/companies">Companies</Button>
+          <Button component={Link} to="/companiesform">Companies Form</Button>
+          <Button component={Link} to="/companiesadmin">Admin View</Button>
+          <Button component={Link} to="/events">EVENTS</Button>
+          <Button component={Link} to="/registerbasic">PROFILE</Button>
+          <Button component={Link} to="/registerEWI">PROFILE(EWI)</Button>
+          <Button component={Link} to="/users/admin">PROFILE SUMMARY(ADMIN)</Button>
+          <Button component={Link} to="/login" color="primary" variant="outlined">Login</Button>
+        </Toolbar>
+      </AppBar>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+            Example Forms
+          </Typography>
+          <Button href="/">Home</Button>
+          <Button component={Link} to="/checkout">Checkout</Button>
+          <Button component={Link} to="/post">POST</Button>
+          <Button component={Link} to="/put">PUT</Button>
+          <Button component={Link} to="/delete">DELETE</Button>
+          <Button component={Link} to="/companies">Companies</Button>
+          <Button component={Link} to="/companiesform">Companies Form</Button>
+          <Button component={Link} to="/companiesadmin">Admin View</Button>
+          <Button component={Link} to="/events">EVENTS</Button>
+          <Button component={Link} to="/registerbasic">PROFILE</Button>
+          <Button component={Link} to="/registerEWI">PROFILE(EWI)</Button>
+          <Button component={Link} to="/login" color="primary" variant="outlined">Login</Button>
+        </Toolbar>
+      </AppBar>
+      </div>
+    )
+  }
 }
 export default withStyles(NavBarStyles)(NavBar);
---
+++ client/src/utils/user-role-hooks.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useAdmin = initialState => {
+	const [admin, setAdmin] = useState(null);
+
+	useEffect(() => {
+		axios.get('/users/session')
+		.then(result => {
+			result.data.forEach(function(user) {
+				axios.get('/users/'+ user.id + '/id')
+					.then(result => {
+						result.data.forEach(function(user) {
+							setAdmin(user.is_admin)
+						})
+					})
+					.catch(err => console.log(err));
+			})
+		})
+		.catch(err => console.log(err));
+		return () => {
+			setAdmin({});
+		};
+	}, []);
+
+	console.log(admin)
+
+	return {
+		admin,
+	};
+};
