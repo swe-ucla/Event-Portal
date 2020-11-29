b314d3a30858dd0ef63ed9409996c63c5293a5e8
nikhitashanker <rocketns@gmail.com>
2020-11-29 08:33:55 -0800

    Add login form.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = (props) => {
         <Button component={Link} to="/registerbasic">PROFILE</Button>
         <Button component={Link} to="/registerEWI">PROFILE(EWI)</Button>
         <Button component={Link} to="/users/admin">PROFILE SUMMARY(ADMIN)</Button>
-        <Button color="primary" variant="outlined">
+        <Button component={Link} to="/login" color="primary" variant="outlined">
           Login
         </Button>
       </Toolbar>
